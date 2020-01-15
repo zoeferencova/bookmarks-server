bc123860c550b829354b6444fbcc1634bf5717b8
zoeferencova <zoeferencova@gmail.com>
2020-01-15 10:22:26 -0500

    updated test file to include id again

--- test/app.spec.js
+++ test/app.spec.js
@@ -1,7 +1,7 @@
 const { expect } = require('chai');
 const knex = require('knex');
 const app = require('../src/app');
-const { makeBookmarksArray } = require('./bookmarks-fixtures');
+const { makeBookmarksArray } = require('./bookmarks.fixtures');
 
 let db;
 
